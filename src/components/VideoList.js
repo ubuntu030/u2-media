ea16b338c4a7c0cc41d6d2cddfb820c6c33c4fe4
ubuntu030 <ubuntu030@gmail.com>
2021-02-22 14:51:08 +0800

    click youtube icon will open youtube ifram

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { getPageVideos, addFavorite } from "../store";
 
+import IframWindow from "./IframWindow";
 import "./videoList.scss";
 
 const VideoList = () => {
 	const dispatch = useDispatch();
 	const videoList = useSelector(getPageVideos);
+	const [isShowIfram, setIsShowIfram] = useState(false);
+	const [videoId, setVideoId] = useState('');
 
 	const handleAddFavorite = (item) => {
 		// console.log(item);
 		dispatch(addFavorite(item));
 	}
+	const openIfram = id => {
+		setVideoId(id);
+		setIsShowIfram(true);
+	}
 	return (
-		<div className="video-list-container">
-			<div className="gallery">
-				{
-					videoList.map(info => <Card key={info.id} info={info} favoriteCb={handleAddFavorite} />)
-				}
+		<>
+			<div className="video-list-container">
+				<div className="gallery">
+					{
+						videoList.map(info => <Card key={info.id} info={info} favoriteCb={handleAddFavorite} openIframCb={openIfram} />)
+					}
+				</div>
 			</div>
-		</div>
+			<IframWindow display={isShowIfram} id={videoId} closeCb={() => setIsShowIfram(false)} />
+		</>
 	)
 }
 
 const Card = (props) => {
 	const { id, title, description, thumbnails } = props.info;
-	const { favoriteCb } = props;
+	const { favoriteCb, openIframCb } = props;
 	return (
 		<div className="card">
 			<div className="card-img">
@@ -38,6 +48,9 @@ const Card = (props) => {
 			</div>
 			<div className="card-desc">
 				<p>{description}</p>
+			</div>
+			<div className="tool-bar">
+				<img src="src/public/icons8-youtube.svg" alt="youtube" onClick={() => openIframCb(id)} />
 			</div>
 		</div>
 	)
