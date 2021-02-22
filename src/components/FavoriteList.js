739148ed72e2ce4e0b6a2d853d2944a454141dab
ubuntu030 <ubuntu030@gmail.com>
2021-02-22 09:18:55 +0800

    FavoriteList component

---
+++ src/components/FavoriteList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+const FavoriteList = () => {
+	return (
+		<section className="favorite-list">
+			
+		</section>
+	)
+}
+
+export default FavoriteList;
