341d642efada10a0b20b8feca19cf1c8282b12cd
ubuntu030 <ubuntu030@gmail.com>
2021-02-22 10:30:07 +0800

    add toggle function update style

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { getFavoriteList, getIsFavoriteOpen } from "../store";
+import { setFavoriteDisplay, delFavoFrite } from "../store";
 
+import "./favoriteList.scss"
 
 const FavoriteList = () => {
+	const dispatch = useDispatch();
+	const favoriteList = useSelector(getFavoriteList);
+	const isOpen = useSelector(getIsFavoriteOpen);
+	const handleDisplayClick = () => {
+		dispatch(setFavoriteDisplay(!isOpen));
+	}
+
 	return (
-		<section className="favorite-list">
-			
-		</section>
+		<>
+			<div onClick={handleDisplayClick} className="favorite-display-icon">
+				Favorite!
+			</div>
+			<section className={`favorite-list-container ${isOpen ? '' : 'hidden'}`}>
+				<div className="favorite-list" >
+
+				</div>
+			</section>
+		</>
 	)
 }
 
