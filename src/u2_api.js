c44859fc043b3bdca8e6564de073896629010950
ubuntu030 <ubuntu030@gmail.com>
2021-02-23 15:17:43 +0800

    param pageToken

--- src/u2_api.js
+++ src/u2_api.js
@@ -1,12 +1,15 @@
 import { YOUTUBE_API_KEY } from "./KEY";
 
-export const fetchVideoList = async () => {
-	const param = 'part=snippet&maxResults=5&type=video&q=surfing&videoDuration=short';
+export const fetchVideoList = async (nextPageToken) => {
+	const param = `part=snippet&maxResults=50&type=video&q=surfing&videoDuration=short${'&pageToken=' + nextPageToken}`;
 	const url = `https://youtube.googleapis.com/youtube/v3/search?${param}&key=${YOUTUBE_API_KEY}`;
 	// https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=YOUR_API_KEY
+	const testurl = 'src/public/test.json';
 	try {
 		const result = await (await fetch(url)).json();
 		console.log(result);
+		// const data = JSON.stringify(result)
+		// console.log(data);
 		if (result && result.error) {
 			throw result.error;
 		}
